90f361363d5e3d3de693621fce77f690327db5fe
samanpwbb <samanpwbb@gmail.com>
2014-11-04 14:45:16 -0500

    reorganize

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
+require('jquery');
+require('mapbox.js');
 var _ = require('underscore');
-var $ = require('jquery');
 var locationFilter = require('leaflet.locationfilter');
 var abaculus = require('abaculus');
 
-$(function() {
 
-    L.mapbox.accessToken = 'pk.eyJ1Ijoic2FtYW4iLCJhIjoiS1ptdnd0VSJ9.19qza-F_vXkgpnh80oZJww';
-    var map = L.mapbox.map('map', 'saman.2os3v7vi');
+L.mapbox.accessToken = 'pk.eyJ1Ijoic2FtYW4iLCJhIjoiS1ptdnd0VSJ9.19qza-F_vXkgpnh80oZJww';
+var map = L.mapbox.map('map', 'saman.2os3v7vi');
 
-    var locationFilter = new L.LocationFilter().addTo(map);
+var locationFilter = new L.LocationFilter().addTo(map);
 
-    locationFilter.on("change", function (e) {
-        // Do something when the bounds change.
-        // Bounds are available in `e.bounds`.
-    });
+locationFilter.on("change", function (e) {
+    // Do something when the bounds change.
+    // Bounds are available in `e.bounds`.
+});
 
-    locationFilter.on("enabled", function () {
-        // Do something when enabled.
-    });
+locationFilter.on("enabled", function () {
+    // Do something when enabled.
+});
 
-    locationFilter.on("disabled", function () {
-        // Do something when disabled.
-    });
+locationFilter.on("disabled", function () {
+    // Do something when disabled.
 });
